9296fa432cf1cb807c869803ac86b09e9b05baf4
KennethStoneP <kennethpv1450@gmail.com>
2024-06-19 11:23:40 -0600

    LOSTANDFOUND

---
+++ src/app/pages/lost-items/lost-and-found/lost-and-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lost-and-found',
+  standalone: true,
+  imports: [],
+  templateUrl: './lost-and-found.component.html',
+  styleUrl: './lost-and-found.component.css'
+})
+export class LostAndFoundComponent {
+
+}
---
+++ src/app/pages/lost-items/lost-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lost-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './lost-items.component.html',
+  styleUrl: './lost-items.component.css'
+})
+export class LostItemsComponent {
+
+}
