a582be93a9dac64425adbb8f5cd4827c7bec3fe3
Joxini <jocksanvargas15@gmail.com>
2024-06-27 01:41:27 -0600

    Avance de corrección de errores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,6 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ZonasComponent } from './zonas/zonas.component';
 import { ModuleObjectsComponent } from './module-objects/module-objects.component';
-//import { ProductDetailGuard } from './Product/product-detail/product-detail.guard';
-//import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path:'home', component: HomeComponent},
--- src/app/zone.service.ts
+++ src/app/zone.service.ts
@@ -2,30 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ZoneService {
-  private apiUrl = 'http://127.0.0.1:5001/zone';
+  private ZoneUrl = (environment.URL_ZONE !== "") ? environment.URL_ZONE : 'api/zones/';
 
   constructor(private http: HttpClient) {}
 
   getZonas(): Observable<any[]> {
-    return this.http.get<any>(this.apiUrl).pipe(
+    return this.http.get<any>(this.ZoneUrl).pipe(
       map(response => response.data)
     );
   }
 
   agregarZona(zona: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, zona);
+    return this.http.post<any>(this.ZoneUrl, zona);
   }
 
   eliminarZona(id: string): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+    return this.http.delete<any>(`${this.ZoneUrl}/${id}`);
   }
 
   actualizarZona(zona: any): Observable<any> {
-    return this.http.put<any>(this.apiUrl, zona);
+    return this.http.put<any>(this.ZoneUrl, zona);
   }
 }
