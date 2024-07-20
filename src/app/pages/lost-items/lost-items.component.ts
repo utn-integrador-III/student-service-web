4cf6c57cc3ccfbbaa224f00a50ef2c9a483a3643
KennethStoneP <kennethpv1450@gmail.com>
2024-07-19 18:40:35 -0600

    Update methtods

--- src/app/pages/lost-items/lost-items.component.ts
+++ src/app/pages/lost-items/lost-items.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
+  selector: 'app-lost-items',
   templateUrl: './lost-items.component.html',
-  styleUrl: './lost-items.component.css',
+  styleUrls: ['./lost-items.component.css'],
 })
-export class LostItemsComponent {}
+export class LostItemsComponent {
+  constructor(
+    public dialogRef: MatDialogRef<LostItemsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
