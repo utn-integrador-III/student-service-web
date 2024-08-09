61dcec743a10612d028b81cc32a1acfbf14a7569
edduardaq <andresquiros.ed@gmail.com>
2024-08-09 17:44:15 -0600

    Subida actualizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,9 +21,7 @@ import { studentLogModule } from './pages/student-log/student-log.module';
 import { homeModule } from './home/home.module';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-<<<<<<< HEAD
 import { EnrollmentModule } from './enrollment/enrollment.module';
-=======
 import { AuthInterceptor } from './auth/auth.interceptor';
 import { appReducer } from './store/app.reducer';
 import { CommonModule } from '@angular/common';
@@ -31,7 +29,6 @@ import { TeacherLogModule } from './pages/teacher-log/teacher-log.module';
 import { PickClassesModule } from './pages/pick-classes/pick-classes.module';
 import { ReportIssueModule } from './pages/report-issues/report-issue/report-issue.module';
 import { VisualizationIssuesModule } from './pages/visualization-issues/visualization-issues.module';
->>>>>>> 5c486d6a180ca5ef456d79bc2afbcc601c35c8c1
 
 @NgModule({
   declarations: [
@@ -60,11 +57,7 @@ import { VisualizationIssuesModule } from './pages/visualization-issues/visualiz
     VisualizationIssuesModule,
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
-<<<<<<< HEAD
     EnrollmentModule
-=======
-    CommonModule,
->>>>>>> 5c486d6a180ca5ef456d79bc2afbcc601c35c8c1
   ],
   providers: [
     provideToastr(),
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,14 +8,11 @@ import { LoginComponent } from './login/login.component';
 import { ZonasComponent } from './zonas/zonas.component';
 import { TeacherLogComponent } from './pages/teacher-log/teacher-log.component';
 import { StudentLogComponent } from './pages/student-log/student-log.component';
-<<<<<<< HEAD
 import { EnrollmentComponent } from './enrollment/enrollment.component';
-=======
 import { AuthGuard } from './auth/auth.guard';
 import { ReportIssueComponent } from './pages/report-issues/report-issue/report-issue.component';
 import { VisualizationIssuesComponent } from './pages/visualization-issues/visualization-issues.component';
 import { PickClassesComponent } from './pages/pick-classes/pick-classes.component';
->>>>>>> 5c486d6a180ca5ef456d79bc2afbcc601c35c8c1
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -23,14 +20,12 @@ export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'example', component: ExampleComponent },
   { path: 'lostAndFound', component: LostAndFoundComponent },
-<<<<<<< HEAD
   { path: 'teacherlog', component: TeacherLogComponent },
   { path: 'studentlog', component: StudentLogComponent },
   { path: 'zones', component: ZonasComponent },
   { path: 'categorias', component: CategoriasComponent },
   { path: 'enroll', component: EnrollmentComponent },
 
-=======
   {
     path: 'teacherlog',
     component: TeacherLogComponent,
@@ -51,7 +46,6 @@ export const routes: Routes = [
   { path: 'reportIssues', component: ReportIssueComponent },
   { path: 'issues', component: VisualizationIssuesComponent },
   { path: 'classes', component: PickClassesComponent },
->>>>>>> 5c486d6a180ca5ef456d79bc2afbcc601c35c8c1
 ];
 
 @NgModule({
