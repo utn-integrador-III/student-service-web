79d410de8cab99357c8c249a975693198f2dc7be
edduardaq <andresquiros.ed@gmail.com>
2024-08-16 18:15:24 -0600

    agregando redireccion a el reseteo

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -23,7 +23,7 @@ export class ResetPasswordComponent {
         next: (response) => {
           // Mostrar un alert y redirigir
           alert('Proceso de restablecimiento iniciado. Revisa tu correo. Seras redirigido a una nueva pàgina donde podràs usar tu contraseña temporal para cambiarla por una nueva');
-          this.router.navigate(['/zones']);  // Redirigir al usuario después de confirmar el alert
+          this.router.navigate(['/cambiocontraseña']);  // Redirigir al usuario después de confirmar el alert
         },
         error: (error) => {
           alert('Error al restablecer la contraseña. Inténtalo de nuevo. Revisa que el correo sea vàlido');
