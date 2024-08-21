3777db23d80d4c62c8234f54f6a6ba97727d7f6c
Remma27 <emmanuelrsolano27@gmail.com>
2024-08-20 18:33:34 -0600

    feat: Refactor role model constructor for improved readability and maintainability

--- src/app/login/models/role.model.ts
+++ src/app/login/models/role.model.ts
@@ -1,6 +1,21 @@
 export class IRole {
-  name: string = '';
-  permissions: string[] = [];
-  is_active: boolean = true;
-  screens: string[] = [];
+  _id?: string = '';
+  name?: string = '';
+  permissions?: string[] = [];
+  is_active?: boolean = true;
+  screens?: string[] = [];
+
+  constructor(
+    _id: string = '',
+    name: string = '',
+    permissions: string[] = [],
+    is_active: boolean = true,
+    screens: string[] = []
+  ) {
+    this._id = _id;
+    this.name = name;
+    this.permissions = permissions;
+    this.is_active = is_active;
+    this.screens = screens;
+  }
 }
